e0f42dc35fd6c7cc5b67a1415995a82f5dda93da
Sarah H <sarahrose.henderson@gmail.com>
2019-09-16 22:32:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 class Dog
-def initialize(name)
-  @name = name
-end
-# def name=(name)
-#   @name = name
-# end
-def name
-  @name
-end
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
 end
